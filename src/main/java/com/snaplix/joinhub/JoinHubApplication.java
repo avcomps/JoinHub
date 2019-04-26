b2ebf7a5319fa3f8de05391c8b94209be3bba3d1
avcomps <aasaidi54@gmail.com>
2019-04-26 20:41:14 +0200

    Main JoinHub Spring project created

---
+++ src/main/java/com/snaplix/joinhub/JoinHubApplication.java
@@ -0,0 +1,13 @@
+package com.snaplix.joinhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snaplix/joinhub/JoinHubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snaplix.joinhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
